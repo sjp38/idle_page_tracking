465ee94fb4013d751f9e585317f3378daf235f8e
SeongJae Park <sj38.park@gmail.com>
2017-09-06 14:11:46 +0900

    pageidle: Design interface
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- pageidle.c
+++ pageidle.c
@@ -1,7 +1,29 @@
+#include <err.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
+void setidle(int start_pfn, int end_pfn)
 {
-	printf("hello\n");
+}
+
+void getidle(int start_pfn, int end_pfn)
+{
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 4)
+		errx(1, "Usage: %s <set|get> <start pfn> <end pfn>\n",
+				argv[0]);
+
+	if (strncmp(argv[1], "set", strlen("set")) == 0)
+		setidle(atoi(argv[2]), atoi(argv[3]));
+	else if (strncmp(argv[1], "get", strlen("get")) == 0)
+		getidle(atoi(argv[2]), atoi(argv[3]));
+	else
+		errx(1, "Usage: %s <set|get> <start pfn> <end pfn>\n",
+				argv[0]);
+
 	return 0;
 }
