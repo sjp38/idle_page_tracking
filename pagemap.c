fc7d5100aa99cf1d905044223c87d3105254e78b
SeongJae Park <sj38.park@gmail.com>
2017-09-06 12:58:26 +0900

    pagemap: Add usage
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- pagemap.c
+++ pagemap.c
@@ -1,7 +1,10 @@
+#include <err.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	if (argc < 2)
+		errx(1, "Usage: %s <pid of process>\n", argv[0]);
 	printf("hello\n");
 
 	return 0;
