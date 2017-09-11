67f80e7a713856650faf088f1b5c473f6335562a
SeongJae Park <sj38.park@gmail.com>
2017-09-11 19:42:25 +0900

    pfnof.py: Receive vaddr range
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- pfnof.py
+++ pfnof.py
@@ -5,22 +5,27 @@ import struct
 import sys
 
 if len(sys.argv) < 3:
-    print "Usage: %s <pid> <vaddr> [vaddr2] ..." % (sys.argv[0])
+    print "Usage: %s <pid> <start vaddr>-<end vaddr>" % (sys.argv[0])
     exit(1)
 
 PAGE_SHIFT = 12
 PAGE_SIZE = 1 << PAGE_SHIFT
 
 pid = sys.argv[1]
-vaddrs = [int(x, 0) for x in sys.argv[2:]]
+va_range = sys.argv[2].split('-')
+start_va = int(va_range[0], 0)
+end_va = int(va_range[1], 0)
 
 def PAGEMAP_PFN(ent):
     return ent & ((1 << 55) - 1)
 
 
 with open("/proc/%s/pagemap" % pid, 'rb') as f:
-    for vaddr in vaddrs:
+    vaddr = start_va
+    while vaddr <= end_va:
         offset = vaddr >> (PAGE_SHIFT - 3)
+        vaddr += PAGE_SIZE
+
         f.seek(offset, 0)
         ent = f.read(8)
         ent = struct.unpack("<Q", ent)[0]
