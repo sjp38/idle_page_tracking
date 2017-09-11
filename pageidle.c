62b8fe64309953a82bfc8d5822b4ca7b368494fe
SeongJae Park <sj38.park@gmail.com>
2017-09-11 20:00:47 +0900

    pageidle: Receive pfns rather than range
    Because virtual memory can be mapped to random physical pages,
    contiguous physical pages for a virtual memory area is not common
    while pageidle receives contiguous range of physical pages as argument.
    To handle the case gracefully, this commit change pageidle input format
    to receive list of page frame numbers instead of contiguous physical
    page range.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- pageidle.c
+++ pageidle.c
@@ -10,17 +10,19 @@
 #define BIT_AT(val, x)	(((val) & (1ull << x)) != 0)
 #define SET_BIT(val, x) ((val) | (1ull << x))
 
-void setidle(int start_pfn, int end_pfn)
+void setidle(int nr_pfns, int pfns[])
 {
 	int fd;
 	int pfn;
 	unsigned long long entry;
+	int i;
 
 	fd = open("/sys/kernel/mm/page_idle/bitmap", O_RDWR);
 	if (fd < 0)
 		err(2, "open bitmap");
 
-	for (pfn = start_pfn; pfn <= end_pfn; pfn++) {
+	for (i = 0; i < nr_pfns; i++) {
+		pfn = pfns[i];
 		entry = 0;
 		if (pread(fd, &entry, sizeof(entry), pfn / 64 * 8)
 				!= sizeof(entry))
@@ -34,17 +36,19 @@ void setidle(int start_pfn, int end_pfn)
 	close(fd);
 }
 
-void getidle(int start_pfn, int end_pfn)
+void getidle(int nr_pfns, int pfns[])
 {
 	int fd;
 	unsigned long long entry;
 	unsigned long long pfn;
+	int i;
 
 	fd = open("/sys/kernel/mm/page_idle/bitmap", O_RDONLY);
 	if (fd < 0)
 		err(2, "open bitmap");
 
-	for (pfn = start_pfn; pfn <= end_pfn; pfn++) {
+	for (i = 0; i < nr_pfns; i++) {
+		pfn = pfns[i];
 		entry = 0;
 		if (pread(fd, &entry, sizeof(entry), pfn / 64 * 8)
 				!= sizeof(entry))
@@ -57,14 +61,23 @@ void getidle(int start_pfn, int end_pfn)
 
 int main(int argc, char *argv[])
 {
+	int *pfns;
+	int nr_pfns;
+	int i;
+
 	if (argc < 4)
-		errx(1, "Usage: %s <set|get> <start pfn> <end pfn>\n",
+		errx(1, "Usage: %s <set|get> <pfn> [pfn 2]...\n",
 				argv[0]);
 
+	nr_pfns = argc - 2;
+	pfns = (int *)malloc(sizeof(int) * nr_pfns);
+	for (i = 0; i < nr_pfns; i++)
+		pfns[i] = atoi(argv[i + 2]);
+
 	if (strncmp(argv[1], "set", strlen("set")) == 0)
-		setidle(atoi(argv[2]), atoi(argv[3]));
+		setidle(nr_pfns, pfns);
 	else if (strncmp(argv[1], "get", strlen("get")) == 0)
-		getidle(atoi(argv[2]), atoi(argv[3]));
+		getidle(nr_pfns, pfns);
 	else
 		errx(1, "Usage: %s <set|get> <start pfn> <end pfn>\n",
 				argv[0]);
