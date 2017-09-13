215f582ae873f7bd980b116ba83632339f0e6ad8
SeongJae Park <sj38.park@gmail.com>
2017-09-14 06:46:37 +0900

    pfnofmap: Add script for pfn of specific mapping
    Heap is the only region that wspagesof.py checks.  It would be useful
    for programs using malloc() only.  However, there are many programs
    using mmap() or other ways to allocate and access memory.  The problem
    comes from `heapof.py` usage rather than entire design.  In other words,
    it will be solved once we can make `stackof.py`, `anonymouspagesof.py`
    like alternatives, making such a tool for every mapping would definitely
    makes no sense, though.  This commit is a beginning point to overcome
    the problem.  It adds a script that receives mapping name and printing
    out pfns of the mapping region.  It can substitute combination of
    `heapof.py` and `pfnof.py`.  Just `pfnofmap.py [heap]` will smoothly
    substitute every use of `heapof.py`-`pfnof.py` combination.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ pfnofmap.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import os
+import struct
+import sys
+
+if len(sys.argv) < 3:
+    print "Usage: %s <pid> <mapping region>" % (sys.argv[0])
+    print "\t`mapping region` is same as path field of /proc/pid/maps`"
+    print "\tUse `anon` as mapping region for anonymous region"
+    exit(1)
+
+PAGE_SHIFT = 12
+PAGE_SIZE = 1 << PAGE_SHIFT
+
+def vaof(pid, region):
+    vas=[]
+    with open("/proc/%s/maps" % pid, 'r') as f:
+        for l in f:
+            splts = l.split()
+            va_range = [int("0x%s" % x, 0) for x in splts[0].split('-')]
+            if len(splts) == 5 and region == "anon":
+                vas.append(va_range)
+            elif len(splts) == 6 and splts[5] == region:
+                vas.append(va_range)
+
+    collapsed_vas = [vas[0]]
+    for r in vas[1:]:
+        if collapsed_vas[-1][1] == r[0]:
+            collapsed_vas[-1][1] = r[1]
+        else:
+            collapsed_vas.append(r)
+    return collapsed_vas
+
+def pr_vas(vas):
+    for r in vas:
+        print "%x-%x" % (r[0], r[1])
+
+def PAGEMAP_PFN(ent):
+    return ent & ((1 << 55) - 1)
+
+
+pid = sys.argv[1]
+region = sys.argv[2]
+
+vas = vaof(pid, region)
+
+with open("/proc/%s/pagemap" % pid, 'rb') as f:
+    for r in vas:
+        start_va = r[0]
+        end_va = r[1]
+
+        vaddr = start_va
+        while vaddr <= end_va:
+            offset = vaddr >> (PAGE_SHIFT - 3)
+            vaddr += PAGE_SIZE
+
+            f.seek(offset, 0)
+            ent = f.read(8)
+            ent = struct.unpack("Q", ent)[0]
+            pfn = PAGEMAP_PFN(ent)
+            if pfn == 0:
+                continue
+            print "%d" % pfn
