d750782b9a2aac5ed9bb6eb7e975a34982034a96
SeongJae Park <sj38.park@gmail.com>
2017-09-14 14:20:51 +0900

    gen_pfns_bin: Allow users to specify mapping region
    Heap, stack, and anonymous pages are hard-coded to be used as target
    memory area in gen_pfns.bin.sh now.  Let user optionally gives specific
    target mapping area by this commit and leave the default behavior to
    work as heap, stack, and anonymous pages yet for backward compatibility.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- gen_pfns_bin.sh
+++ gen_pfns_bin.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+if [ $# -lt 2 ]
 then
 	echo $# $0 $1 $2
-	echo "Usage: $0 <pid> <file name>"
+	echo "Usage: $0 <pid> <file name> [mapping region]"
 	exit 1
 fi
 
 pid=$1
 pfns_file=$2
+mregions="[heap],[stack],anon"
+if [ $# -gt 2 ]
+then
+	mregions=$3
+fi
 
-pfns=`sudo ./pfnofmap.py $pid "[heap],[stack],anon"`
+pfns=`sudo ./pfnofmap.py $pid $mregions`
 echo $pfns | ./u8lst_txt2bin.py $pfns_file
