9bc403c30318903d3165d9c06d6bb07999448581
SeongJae Park <sj38.park@gmail.com>
2018-01-24 18:21:06 +0900

    wspagesof: Add runtime profiling feature
    This commit adds a simple runtime profiling feature in wspagesof.sh
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- wspagesof.sh
+++ wspagesof.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PROFILE=
+
 if [ $# -ne 2 ]
 then
 	echo "Usage: $0 <pfns file> <interval>"
@@ -9,7 +11,16 @@ fi
 pfns_file=$1
 interval=$2
 
+if [ $PROFILE ]; then SECONDS=0; fi
 sudo ./pageidle set $pfns_file
+if [ $PROFILE ]; then echo "$SECONDS secs for setting pages idle"; fi
+
 sleep $interval
+
+if [ $PROFILE ]; then SECONDS=0; fi
 bits=`sudo ./pageidle get $pfns_file`
+if [ $PROFILE ]; then echo "$SECONDS secs for getting pages idle"; fi
+
+if [ $PROFILE ]; then SECONDS=0; fi
 grep -o "0" <<< $bits | wc -l
+if [ $PROFILE ]; then echo "$SECONDS secs for couting working set"; fi
